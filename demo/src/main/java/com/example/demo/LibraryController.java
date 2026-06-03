f36bfa23f09d835ab792fb5c3107b271c80db35a
Shahd <shahad9mubarak@gmail.com>
2026-06-03 11:32:34 +0400

    declare two separate, private, static storage array lists

--- demo/src/main/java/com/example/demo/LibraryController.java
+++ demo/src/main/java/com/example/demo/LibraryController.java
@@ -2,7 +2,11 @@ package com.example.demo;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 
 public class LibraryController {
+    private  static List<Author> authorList;
+    private static List<Book> bookList;
 }
