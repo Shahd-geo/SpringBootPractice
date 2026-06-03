617b81c28beae5c2bd947b53da625d0f0feae372
Shahd <shahad9mubarak@gmail.com>
2026-06-03 14:40:12 +0400

    ADD InventoryBook LIST

--- demo/src/main/java/com/example/demo/BookstoreController.java
+++ demo/src/main/java/com/example/demo/BookstoreController.java
@@ -1,8 +1,24 @@
 package com.example.demo;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class BookstoreController {
+    private static List<InventoryBook> bookstore=new ArrayList<>();
+
+
+
+
+    @GetMapping("/addIteam")
+    public static List<InventoryBook> bookstore(@RequestParam int id,@RequestParam String titel , @RequestParam int price , int stockCount ){
+
+    }
+
+
 }
