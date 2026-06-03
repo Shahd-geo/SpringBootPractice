4b90d80c8061c7888a579976125e98fc2fc0910b
Shahd <shahad9mubarak@gmail.com>
2026-06-03 11:41:10 +0400

    Create a basic GET endpoint mapped to /add-author

--- demo/src/main/java/com/example/demo/LibraryController.java
+++ demo/src/main/java/com/example/demo/LibraryController.java
@@ -1,5 +1,7 @@
 package com.example.demo;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -9,4 +11,16 @@ import java.util.List;
 public class LibraryController {
     private  static List<Author> authorList;
     private static List<Book> bookList;
+
+    @GetMapping("/addAuthor")
+    public String ADDAUTHOR( @RequestParam int id,  @RequestParam String name, @RequestParam String biography) {
+        Author newAuthor = new Author(id, name, biography);
+        authorList.add(newAuthor);
+        return "Author added successfully! ID: " + id + ", Name: " + name;
+    }
+    @GetMapping("/allAuthors")
+    public List<Author> getAllAuthors() {
+        return authorList;
+    }
 }
+
