67badf39941c28c923fd8800a982d61852321a6c
Shahd <shahad9mubarak@gmail.com>
2026-06-03 11:57:48 +0400

    The Master Ecosystem Controller

--- demo/src/main/java/com/example/demo/LibraryController.java
+++ demo/src/main/java/com/example/demo/LibraryController.java
@@ -4,13 +4,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 
 public class LibraryController {
-    private  static List<Author> authorList;
-    private static List<Book> bookList;
+    private  static List<Author> authorList = new ArrayList<>();
+    private static List<Book> bookList =new ArrayList<>();
 
     @GetMapping("/addAuthor")
     public String ADDAUTHOR( @RequestParam int id,  @RequestParam String name, @RequestParam String biography) {
