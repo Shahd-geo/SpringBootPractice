01ab6b2f31bc6ba4379928d777e1e952d112adc0
Shahd <shahad9mubarak@gmail.com>
2026-06-03 09:27:42 +0400

    Generate a constructor

--- demo/src/main/java/com/example/demo/Book.java
+++ demo/src/main/java/com/example/demo/Book.java
@@ -2,26 +2,11 @@ package com.example.demo;
 
 public class Book {
     private String BookName;
-    private int id ;
+    private int id;
 
-    public String getBookName() {
-        return BookName;
-    }
-
-    public void setBookName(String bookName) {
-        BookName = bookName;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public Book(String bookName, String id) {
+    public Book(String bookName, int id) {
         BookName = bookName;
         this.id = id;
     }
+
 }
